9ecc64e8ab8efe8e8878bfd3e3c539ccd0b89679
Ravshan <ravkart@gmail.com>
2019-09-13 13:10:03 -0700

    added shop data

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import SHOP_DATA from './shop.data'
+
+class ShopPage extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+}
