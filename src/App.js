ca148f72ea64fa520d11cdfbca1d5778a02e2856
Ravshan <ravkart@gmail.com>
2019-09-13 13:49:35 -0700

    added shop component preview and item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Homepage from './pages/homepage/homepage.component'
+import {Switch, Route} from 'react-router-dom'
+
+import ShopPage from './pages/shop/shop.component'
+import HomePage from './pages/homepage/homepage.component'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
