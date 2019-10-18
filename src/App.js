5829d68f317f60489664e501ebb72dd44fb410e8
Ravshan <ravkart@gmail.com>
2019-10-17 17:45:43 -0700

    implemented pull in shop data from firestore, implemented hoc with spinner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom'
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import ShopPage from './pages/shop/shop.component'
 import HomePage from './pages/homepage/homepage.component'
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
+import { selectCurrentUser } from './redux/user/user.selectors';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 import './App.css'
 
@@ -19,7 +21,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -32,6 +34,9 @@ class App extends React.Component {
         });
       }
       setCurrentUser(userAuth);
+
+    //  addCollectionAndDocuments('collections', 
+    //  collectionsArray.map(({title, items}) => ({title, items})))
     });
   }
 
@@ -64,8 +69,9 @@ class App extends React.Component {
   }
   
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
